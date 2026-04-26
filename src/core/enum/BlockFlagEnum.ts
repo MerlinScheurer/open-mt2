34b41c168ac9baec7e973e8b4da4fcd304e691d1
Merlin Scheurer <merlin.scheurer@gmail.com>
2026-04-26 22:27:33 +0200

    Fix sonarqube issues

--- src/core/enum/BlockFlagEnum.ts
+++ src/core/enum/BlockFlagEnum.ts
@@ -1,9 +1,9 @@
 export enum BlockFlagEnum {
     NONE = 0,
-    EXCHANGE = 1 << 0,
-    PARTY_INVITE = 1 << 1,
-    GUILD_INVITE = 1 << 2,
-    WHISPER = 1 << 3,
-    MESSENGER_INVITE = 1 << 4,
-    PARTY_REQUEST = 1 << 5,
+    EXCHANGE = 1,
+    PARTY_INVITE = 2,
+    GUILD_INVITE = 4,
+    WHISPER = 8,
+    MESSENGER_INVITE = 16,
+    PARTY_REQUEST = 32,
 }
